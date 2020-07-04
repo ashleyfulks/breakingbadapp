c68cbdf7991835e3e6aba1f7909a243d4936ed5a
Ashley Fulks <ashley.fulks@shopify.com>
2020-07-04 11:49:37 -0700

    build

---
+++ build/precache-manifest.f091eb0d143c0bc375e4f6c3a7b2f5da.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "749d3d5595fc6cf3e97ff7d00f77e025",
+    "url": "/index.html"
+  },
+  {
+    "revision": "363d009b623f2fe6a72e",
+    "url": "/static/css/main.aceea456.chunk.css"
+  },
+  {
+    "revision": "b14261e6cb9ed34c1893",
+    "url": "/static/js/2.a659b1fe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.a659b1fe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "363d009b623f2fe6a72e",
+    "url": "/static/js/main.f1c0be23.chunk.js"
+  },
+  {
+    "revision": "fb746c9ad1d6578fa0ab",
+    "url": "/static/js/runtime-main.4ce1a1e1.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/static/media/spinner.235b9b3e.gif"
+  }
+]);
